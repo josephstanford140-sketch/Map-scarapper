17f49f1957c390e08a0380f394cbad818b96efcb
josephstanford140-sketch <josephstanford140@gmail.com>
2026-02-18 21:12:44 +0530

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Dashboard from "@/pages/Dashboard";
+import ScrapeDetails from "@/pages/ScrapeDetails";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Dashboard} />
+      <Route path="/scrapes/:id" component={ScrapeDetails} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
